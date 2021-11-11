b1e202550e05a304e7f02a78bc1c3badd8ce6710
karrot <jayce@karrotui-MacBookPro.local>
2021-11-11 16:58:09 +0900

    goroutine count emulation

---
+++ goroutine-per-gb/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	ch := make(chan struct{})
+	var nch chan struct{}
+
+	cnt := 0
+	total := 0
+
+	go func() {
+		for {
+			<-ch
+			PrintMemUsage(total)
+		}
+	}()
+
+	fmt.Println("Goroutine start")
+
+	for {
+		cnt++
+		go func() {
+			<-nch // block forever
+		}()
+		if cnt == 10000 {
+			total += cnt
+			cnt = 0
+			ch<- struct{}{}
+		}
+	}
+}
+
+// PrintMemUsage outputs the current, total and OS memory being used. As well as the number
+// of garage collection cycles completed.
+func PrintMemUsage(totalCall int) {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("TotalCall %d:\tStackInuse = %.3v MiB\tStackSys = %.3v MiB\tTotalAlloc = %.3v MiB\tTotalAlloc = %.3v MiB\tSys = %.3v MiB\n", totalCall, bToMb(m.StackInuse), bToMb(m.StackSys), bToMb(m.HeapAlloc), bToMb(m.TotalAlloc), bToMb(m.Sys))
+}
+
+func bToMb(b uint64) float64 {
+	return float64(b) / 1024.0 / 1024.0
+}
